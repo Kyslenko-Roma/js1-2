43de1103503dc08c364416154bb757f1461e0532
Kyslenko-Roma <112819019+Kyslenko-Roma@users.noreply.github.com>
2024-01-14 23:53:49 +0200

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,8 @@
+let favouriteTown = prompt("Я люблю місто ____");
+alert("Я люблю місто " + favouriteTown);
+
+let lastYearVisited = prompt("Я там останній раз був о _____ році");
+alert("Я там останній раз був в " + lastYearVisited + " році");
+
+let attractionTown = prompt("Також це місто відомо по ______.");
+alert("Також це місто відомо по " + attractionTown);
